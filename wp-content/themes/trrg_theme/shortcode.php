a47b618fc577c5ce9af336f5142aa3173fd5589c
Kirdric Hill <khill@clickhere.com>
2012-10-12 16:48:55 -0500

    features update

--- wp-content/themes/trrg_theme/shortcode.php
+++ wp-content/themes/trrg_theme/shortcode.php
@@ -1,8 +1,18 @@
 <?php
 
-    add_shortcode('video_embed', function()    {
-        return '<div class="video_image">
-                <iframe width="604" height="320" src="http://www.youtube.com/embed/FzGytsHyr2I" frameborder="0" allowfullscreen></iframe>
+    add_shortcode('video_embed', function($atts)    {
+        $atts = shortcode_atts (
+            array (
+            'src' => '',
+            'width' => 604,
+            'height' => 320,
+            'title' => ''
+            ), $atts);
+
+
+                return '<div class="video_image">
+                <iframe width="' . $atts['width'] . '" height="' .$atts['height']. '" src="' . $atts['src'] . '" frameborder="0" allowfullscreen></iframe>
+                <h4>' .$atts['title'] .'</h4>
             </div>';
 
 
