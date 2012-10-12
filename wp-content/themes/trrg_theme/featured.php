43845caaa9bbb2595f052367f7ab53fbd429447e
Kirdric Hill <khill@clickhere.com>
2012-10-12 11:37:30 -0500

    features update

---
+++ wp-content/themes/trrg_theme/featured.php
@@ -0,0 +1,12 @@
+<div id="featured-th">
+ <div class="grid_3">
+     <p>One</p>
+ </div>
+ <div class="grid_3">
+     <p>Two</p>
+ </div>
+ <div class="grid_2">
+     <p>Three</p>
+ </div>
+
+</div><!--/featured-th-->
