0f33ce94486065e78c311c626025883dbeba4acb
Kirdric Hill <khill@clickhere.com>
2012-10-12 14:40:32 -0500

    features update

--- wp-content/themes/trrg_theme/home.php
+++ wp-content/themes/trrg_theme/home.php
@@ -5,7 +5,7 @@
             	<div id="content" class="content">
                     <!-- Content Entry -->
                     <?php $recent = new WP_Query("cat=1&showposts=1"); while($recent->have_posts()) : $recent->the_post();?>
-                        <h2 class="title"><?php the_title(); ?></h2>
+                        <h3 class="title"><?php the_title(); ?></h3>
                         <?php the_content(__('Read the story &raquo;'));?><div style="clear:both;"></div>
                     <?php endwhile; ?>
 
