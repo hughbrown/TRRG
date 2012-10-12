e71802b99cff41d07fcbd2a65641664817d49d9e
Kirdric Hill <khill@clickhere.com>
2012-10-12 15:32:21 -0500

    features update

--- wp-content/themes/trrg_theme/featured.php
+++ wp-content/themes/trrg_theme/featured.php
@@ -5,8 +5,9 @@
  </div>
  <div class="grid_5 omega">
      <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Column 3 Feature') ) : ?>
+     <?php fb_feed() ?>
      <?php else: ?>
-        <?php fb_feed() ?>
+
      <?php endif; ?>
 
  </div>
