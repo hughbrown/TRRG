edab427710201d91f202b92d1d24d90ec83f2d79
Kirdric Hill <khill@clickhere.com>
2012-10-12 11:49:30 -0500

    features update

--- wp-content/themes/trrg_theme/featured.php
+++ wp-content/themes/trrg_theme/featured.php
@@ -1,11 +1,11 @@
 <div id="featured-th" class="grid_8">
- <div class="grid_3">
+ <div class="grid_3 alpha">
      <p>One</p>
  </div>
  <div class="grid_3">
      <p>Two</p>
  </div>
- <div class="grid_2">
+ <div class="grid_2 omega">
      <p>Three</p>
  </div>
 
