ce43d76499421f24a90e7d667d307b5343ac08c8
Kirdric Hill <khill@clickhere.com>
2012-10-12 16:30:45 -0500

    features update

--- wp-content/themes/trrg_theme/shortcode.php
+++ wp-content/themes/trrg_theme/shortcode.php
@@ -5,5 +5,5 @@
             <div class="video_image">
                 <iframe width="634" height="320" src="http://www.youtube.com/embed/FzGytsHyr2I" frameborder="0" allowfullscreen></iframe>
             </div>
-            '
+            
     });
