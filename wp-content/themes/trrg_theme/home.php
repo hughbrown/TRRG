735c19c98eb9f9d86662e514bef95473a839eadc
Kirdric Hill <khill@clickhere.com>
2012-10-12 14:25:20 -0500

    features update

---
+++ wp-content/themes/trrg_theme/home.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+        <div id="contentwrap" class="grid_12">
+            <?php get_sidebar('left'); ?>
+            <div class="grid_8 trrg-mid">
+            	<div id="content" class="content">
+                    <!-- Content Entry -->
+                    <?php $recent = new WP_Query("cat=1&showposts=1"); while($recent->have_posts()) : $recent->the_post();?>
+                        <h2><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+                        <?php the_content(__('Read the story &raquo;'));?><div style="clear:both;"></div>
+                    <?php endwhile; ?>
+
+
+                    <!-- Content Entry End-->
+
+
+                </div> <!-- content end -->
+                <?php if(is_home()) { include (TEMPLATEPATH . '/featured.php'); } ?>
+            </div> <!-- grid_8 end -->
+            <?php get_sidebar('right'); ?>
+        </div> <!-- contentwrap end -->
+<?php get_footer(); ?>
