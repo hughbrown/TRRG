1deae93bd050d1f458a3cdf45d5a24ba300e7d11
Kirdric Hill <khill@clickhere.com>
2012-10-12 16:29:40 -0500

    features update

---
+++ wp-content/themes/trrg_theme/shortcode.php
@@ -0,0 +1,9 @@
+<?php
+
+    add_shortcode('video_embed', function()    {
+        return '
+            <div class="video_image">
+                <iframe width="634" height="320" src="http://www.youtube.com/embed/FzGytsHyr2I" frameborder="0" allowfullscreen></iframe>
+            </div>
+            '
+    });
